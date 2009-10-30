8cef8b37d0aa00fc8c027252564d6aaa48ee230c
Deng Wei <salala_mud@163.com>
2009-10-31 01:43:00 +0800

    test

--- update.php
+++ update.php
@@ -3,6 +3,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', true);
 
 echo '<pre>git pull' . "\n";
-exec('git pull', $result);
+echo exec('git pull', $result);
 var_dump($result);
 echo 'git finish</pre>';
