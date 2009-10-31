140df28972110fa31a4f91fc3ffb04c943071291
Deng Wei <salala_mud@163.com>
2009-10-31 23:46:06 +0800

    test

--- cls/request.php
+++ cls/request.php
@@ -5,15 +5,23 @@ class cls_request
 	{
 		if (empty($_SERVER["REQUEST_URI"]))
 		{
-			echo 'cli mode.';
+			echo '[MODE] cli-mode running.' . "\n";
+
 			$cli = new cls_cli();
 			$cli->dispatch();
+	
 			return;
 		}
 		
 		$uri = parse_url($_SERVER['REQUEST_URI']);
-		$dirs = explode('/', trim($uri['path'], '/'));
-		var_dump($uri);
-		var_dump($dirs);
+		$dirs = pathinfo(trim($uri['path'], '/'));
+		$controller = 'controller_' . str_replace('/', '_', $dirs['dirname']);
+		$method = $dirs['filename'];
+
+		// $controller = new $controller();
+		// $controller->$method();
+
+		var_dump($uri, $dirs, $controller, $method);
+		// require_once $controller . '.php';
 	}
 }
