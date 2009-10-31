acc2050b7ea41bd2fbf64d6a063cb79fab1538ef
Deng Wei <salala_mud@163.com>
2009-10-31 12:23:46 +0800

    test

--- cls/request.php
+++ cls/request.php
@@ -3,6 +3,15 @@ class cls_request
 {
 	public function dispatch()
 	{
-		phpinfo();
+		if (empty($_SERVER["REQUEST_URI"]))
+		{
+			echo 'cli mode.';
+			$cli = new cls_cli();
+			$cli->dispatch();
+			return;
+		}
+		
+		$uri = parse_url($_SERVER['REQUEST_URI']);
+		var_dump($uri);
 	}
 }
