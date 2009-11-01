dc588eecbb54c724698cd4712a674cb2849803e9
Deng Wei <salala_mud@163.com>
2009-11-02 00:04:53 +0800

    test

--- controller/default.php
+++ controller/default.php
@@ -8,9 +8,12 @@ class controller_default
 	
 	public function update()
 	{
-		echo '<pre>git pull' . "\n";
+		echo 'git pull<br />';
 		exec('/home/vtwoexpc/bin/git pull', $result);
-		var_dump($result);
-		echo 'git finish</pre>';
+		foreach($result as $r)
+		{
+			echo $r . "<br />";
+		}
+		echo 'git finish';
 	}
 }
