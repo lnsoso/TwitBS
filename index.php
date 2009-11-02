f3f26e3669cdbd517596fb4bb523b144035be6b5
Deng Wei <salala_mud@163.com>
2009-11-02 12:24:14 +0800

    test

--- index.php
+++ index.php
@@ -7,5 +7,5 @@ $req->dispatch();
 if (defined('DEBUG') && DEBUG)
 {
 	$debug = new cls_debug();
-	$debug->cost_time('<br />total runtime:', TIME);
+	$debug->cost_time('<br />[TIME] total runtime : ', TIME);
 }
